78e8a4f539babea3e9d715a31682bbeadfd60a4f
islandsexit <hezkydoc@gmail.com>
2022-03-04 17:38:13 +0300

    day1

--- face/face/settings.py
+++ face/face/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'upload_app.apps.UploadAppConfig',
+    'upload_app',
 ]
 
 MIDDLEWARE = [
