1a81a7d470e5490d70b32bb38503829e11800154
islandsexit <hezkydoc@gmail.com>
2022-03-10 12:15:43 +0300

    Добавил все файлы

---
+++ face/upload_app/forms.py
@@ -0,0 +1,20 @@
+from .models import Face
+from django.forms import ModelForm, ImageField,TextInput
+
+
+class FaceFormRegister(ModelForm):
+    class Meta:
+        model = Face
+        fields = ['face_token_ch' ]
+        widgets = {'face_token_ch':TextInput(attrs={
+            'class':'form_face',
+            'placeholder':'********',
+            'maxlength':'6',
+            'pattern':'[0-9]*',
+            'x-inputmode':'numeric',
+            'oninput':'this.value=this.value.replace(/[^0-9]/,&quot;&quot;)',
+            'onkeyup':"this.value = this.value.replace(/[^\d]/g,'');"
+
+            })}
+
+
