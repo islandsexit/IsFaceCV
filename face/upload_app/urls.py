9d8909af8245b206b3bb2b8fbd1f07d493065515
islandsexit <hezkydoc@gmail.com>
2022-03-29 15:19:49 +0300

    back_for_mobile

--- face/upload_app/urls.py
+++ face/upload_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('backendofreact', views.index, name='index'),
     path('checkin', views.auth, name='auth')
     # path('created/', views.added, name='created')
 ]
--- face/upload_app/views.py
+++ face/upload_app/views.py
@@ -1,4 +1,5 @@
 from logging import raiseExceptions
+from random import random
 
 from .modules.face_validation import isFace_in_img
 from .modules.face_validation import img_Base64
@@ -48,27 +49,26 @@ def auth(request):
                     responseServ = responseVov.json()
                     result = responseServ['RESULT']
                     msg = responseServ['DESC']
-                    
-                    return JsonResponse({'result':f'{result}','msg':f'{msg}'})
+
+                    return JsonResponse({'result': f'{result}', 'msg': f'{msg}'})
                     # return render(request, './upload_app/auth.html',
                     #               {'header': str(responseVov.json()) + " img: " + str(name)})
-                    
 
-                except :
+                except:
                     # return render(request, './upload_app/auth.html',
                     #               {'no_face': 'Ошибка на сервере Вовы', "valid": "0", "id": f'{person.id}'})
-                    return JsonResponse({'result':f'ERROR','msg':f'Ошибка на сервере Вовы'})
+                    return JsonResponse({'result': f'ERROR', 'msg': f'Ошибка на сервере Вовы'})
 
             except:
-                
+
                 # return render(request, './upload_app/auth.html',
                 #               {'no_face': 'Ошибка кодирования в Base64', "valid": "0", "id": f'{person.id}'})
-                return JsonResponse({'result':f'ERROR','msg':f'Ошибка кодирования Base64'})
+                return JsonResponse({'result': f'ERROR', 'msg': f'Ошибка кодирования Base64'})
 
         # return render(request, './upload_app/auth.html',
         #               {"name": f"{name}", "valid": "0", "id": f'{person.id}',
         #                "no_face": "На фото не было найдено лицо"})
-        return JsonResponse({'result':f'ERROR','msg':f'На фото не было найдено лицо'})
+        return JsonResponse({'result': f'ERROR', 'msg': f'На фото не было найдено лицо'})
 
         return JsonResponse({'message': f'{request.POST, request.FILES}'})
 
@@ -81,72 +81,38 @@ def auth(request):
         # print(request.GET)
         # если отправился
         if face_token_ch and len(face_token_ch) == 6:
-            #print('a')
+            # print('a')
             try:
                 active, person, name = active_code(face_token_ch)
                 if True:  # active:
                     return render(request, './upload_app/auth.html', {'name': f'{name}', "valid": 'True', "id": f'{person.id}', "password": f'{face_token_ch}'})
                 else:
-                    return render(request, './upload_app/code.html', {'value_pass':'007'})
+                    return render(request, './upload_app/code.html', {'value_pass': '007'})
 
             except:
 
                 return render(request, './upload_app/code.html',
-                              {'value_pass':'007'})
+                              {'value_pass': '007'})
 
         else:
-            return render(request, './upload_app/code.html', {'value_pass':''})
-
-    # ---------------------POST----------------------------
-    # if request.method == 'POST':
-    #     face_token_ch = request.POST.get('password')
-    #     ID = request.POST['id']
-    #     name = request.POST.get('name')
-    #     file = request.FILES['file_img']
-    #     print(file)
-    #     try:
-    #         active, person, name = active_code(face_token_ch)
-    #         if False:  # active != True:
-    #             raise Exception('Не активный код')
-
-    #     except e as Exception:
-    #         print(e)
-    #         return render(request, './upload_app/auth.html', {'header': 'ОШИБКА'})
-
-    #     img, confidence = isFace_in_img(file)
-
-    #     if confidence:
-
-    #         try:
-
-    #             img64 = img_Base64(img)
-
-    #             try:
-    #                 responseVov = RQ.post('http://192.168.48.114:8080/docreateguest', data={
-    #                     "ID": person.id,
-    #                     "img64": img64,
-    #                     "name": name
-
-    #                 })
-    #                 print(name)
-    #                 return render(request, './upload_app/auth.html',
-    #                               {'header': str(responseVov.json()) + " img: " + str(name)})
-
-    #             except Exception as e:
-    #                 return render(request, './upload_app/auth.html',
-    #                               {'no_face': 'Ошибка на сервере Вовы', "valid": "0", "id": f'{person.id}'})
-
-    #         except Exception as e:
-    #             print(e)
-    #             return render(request, './upload_app/auth.html',
-    #                           {'no_face': 'Ошибка кодирования в Base64', "valid": "0", "id": f'{person.id}'})
-
-    #     return render(request, './upload_app/auth.html',
-    #                   {"name": f"{name}", "valid": "0", "id": f'{person.id}',
-    #                    "no_face": "На фото не было найдено лицо"})
-
-    # return render(request, './upload_app/auth.html', {'header': 'Введите код приглашения'})
+            return render(request, './upload_app/code.html', {'value_pass': ''})
 
 
 def index(request):
-    return render(request, './upload_app/auth.html', {'header': 'Введите код', "valid": 0})
+    if request.method == 'GET':
+        if request.GET.get('master_password', False) == 'secretmasterpasswordvig':
+            face_token_ch = request.GET.get('invite_code', False)
+            if face_token_ch and len(face_token_ch) == 6:
+                # print('a')
+                try:
+                    active, person, name = active_code(face_token_ch)
+                    if True:  # active:
+                        return JsonResponse({'RESULT': 'SUCCESS', 'code': f'{person.id}'})
+                    else:
+                        return JsonResponse({'RESULT': 'ERROR', 'CODE': f'0'})
+                except Exception as e:
+
+                    return JsonResponse({'RESULT': 'ERROR', 'CODE': f'{e}'})
+        else:
+            return JsonResponse({'RESULT': 'AXAXAXAXAXAXAXXAX', 'CODE': 'Im not vindictive. I will write it down.'})
+    return JsonResponse({'RESULT': 'ERROR', 'CODE': f'0'})
