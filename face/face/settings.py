4bdc1f33ec3bb28f6f977eed99b5e7be074c4971
islandsexit <hezkydoc@gmail.com>
2022-04-26 20:07:40 +0300

    fixed

--- face/face/settings.py
+++ face/face/settings.py
@@ -87,23 +87,27 @@ DATABASES = {
     }
 }
 
+
 LOGGING = {
-'version': 1,
+    'version': 1,
+    # The version number of our log
     'disable_existing_loggers': False,
-        'handlers': {
-        'console': {
-            'formatter': 'default',
-            'class': 'logging.StreamHandler',
-            'stream': 'ext://sys.stderr',
+    # django uses some of its own loggers for internal operations. In case you want to disable them just replace the False above with true.
+    # A handler for WARNING. It is basically writing the WARNING messages into a file called WARNING.log
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': BASE_DIR / 'warning.log',
         },
-        'output': {
-            'level': 'DEBUG',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'formatter': 'default',
-            'filename': 'home/vig/django/IsFaceCV/django.log',
-            'when': 'MIDNIGHT',
-            'interval': 1,
-            'backupCount': 5,
+    },
+    # A logger for WARNING which has a handler called 'file'. A logger can have multiple handler
+    'loggers': {
+       # notice the blank '', Usually you would put built in loggers like django or root here based on your needs
+        '': {
+            'handlers': ['file'], #notice how file variable is called in handler which has been defined above
+            'level': 'WARNING',
+            'propagate': True,
         },
     },
 }
