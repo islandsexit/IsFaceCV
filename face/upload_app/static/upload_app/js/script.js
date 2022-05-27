0a6b1688900e63aca9c2bdfb8cc3676e84a86efb
islandsexit <hezkydoc@gmail.com>
2022-05-27 15:49:44 +0300

    sucess fix

--- face/upload_app/static/upload_app/js/script.js
+++ face/upload_app/static/upload_app/js/script.js
@@ -116,7 +116,7 @@ $(document).ready(function () {
 					// status_bar[0].style.display = 'none';
 					window.console.log(response['result'])
 					window.console.log(response['msg'])
-					if (response['result']=='SUCCES'){
+					if (response['result']=='SUCCESS'){
 					// $("#btn_submit").removeClass("btn-submit")
 					btnOuter.addClass("file_uploaded");
 					error_msg[0].style.color='green'
